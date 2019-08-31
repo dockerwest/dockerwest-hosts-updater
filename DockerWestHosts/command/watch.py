cd4e0af928196bfe3727879a9f07831e93b9ee93
BlackEagle <ike.devolder@gmail.com>
2019-08-31 22:19:09 +0200

    initial working version to update a hosts file
    Using the VIRTUAL_HOST and DOMAIN_NAME env vars passed to the containers
    we can now update the hosts file. Using the dynamic ip adress from the
    docker networks, or if you pass an ip add that one to the given hosts
    file.
    
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- DockerWestHosts/command/watch.py
+++ DockerWestHosts/command/watch.py
@@ -1,4 +1,6 @@
 import argparse
+import mmap
+import fileinput
 import docker
 
 from DockerWestHosts.command.base import CommandBase
@@ -12,6 +14,12 @@ class WatchDockerEvents(CommandBase):
         self.__description = description + " watch"
         self.__file = '/etc/hosts'
         self.__ip = None
+
+        self.__start_marker = '## START %s' % self.__name
+        self.__end_marker = '## END %s' % self.__name
+
+        self.__client = docker.from_env()
+        self.__apiclient = docker.APIClient()
 
         parser = argparse.ArgumentParser(
             description=self.__description,
@@ -31,10 +39,10 @@ class WatchDockerEvents(CommandBase):
         flags = parser.parse_args(args)
 
         if None is not flags.file:
-            self.__file = args.file
+            self.__file = flags.file
 
         if None is not flags.ip:
-            self.__ip = args.ip
+            self.__ip = flags.ip
 
     def run(self):
         ip = 'dynamic'
@@ -43,13 +51,37 @@ class WatchDockerEvents(CommandBase):
 
         print(self.__description)
         print('watching docker events and update hosts with: %s' % ip)
-        client = docker.from_env()
+
+        # check the hosts file for dockerwest markers
+        self.__markers()
+
+        # before listening to events list the currently running containers
+        self.__hosts()
+
+        # listen to events and update hosts
+        self.__listen_for_events()
+
+    def __markers(self):
+        if not self.__check_markers():
+            with open(self.__file, 'a') as hostsfile:
+                hostsfile.write('%s\n' % self.__start_marker)
+                hostsfile.write('%s\n' % self.__end_marker)
+
+    def __check_markers(self):
+        with open(self.__file, 'rb', 0) as file, \
+                mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
+            if s.find(str.encode(self.__start_marker)) != -1:
+                return True
+
+        return False
+
+    def __listen_for_events(self):
         while True:
-            for event in client.events(decode=True):
-                self.handleevent(event)
+            for event in self.__client.events(decode=True):
+                self.__handleevent(event)
                 break
 
-    def handleevent(self, event):
+    def __handleevent(self, event):
         if 'status' in event and \
                 (
                     'start' == event['status']
@@ -57,4 +89,71 @@ class WatchDockerEvents(CommandBase):
                     or 'kill' == event['status']
                     or 'die' == event['status']
                 ):
-            print(event)
+            self.__hosts()
+
+    def __hosts(self):
+        hosts = []
+        containers = self.__client.containers.list()
+        for container in containers:
+            if 'running' == container.status:
+                hostsinfo = self.__hostsinfo(container)
+                if None is not hostsinfo:
+                    hosts += [hostsinfo]
+
+        self.__update_hosts(hosts)
+
+    def __hostsinfo(self, container):
+        hosts = []
+        ips = []
+        container_info = self.__apiclient.inspect_container(container.name)
+        if "Config" in container_info and "Env" in container_info["Config"]:
+            for envvar in container_info["Config"]["Env"]:
+                envvarkey, envvarvalue = envvar.split("=")
+                if "VIRTUAL_HOST" == envvarkey or "DOMAIN_NAME" == envvarkey:
+                    hosts += envvarvalue.split(',')
+        if hosts:
+            if None is self.__ip:
+                if "NetworkSettings" in container_info:
+                    container_nwsettings = container_info["NetworkSettings"]
+                    if "IPAddress" in container_nwsettings \
+                            and "" != container_nwsettings["IPAddress"]:
+                        ips += [container_nwsettings["IPAddress"]]
+                    if "Networks" in container_nwsettings:
+                        nwnetworks = container_nwsettings["Networks"]
+                        for nwname, nwsettings in nwnetworks.items():
+                            if "IPAddress" in nwsettings \
+                                    and "" != nwsettings["IPAddress"]:
+                                ips += [nwsettings["IPAddress"]]
+            else:
+                ips += [self.__ip]
+
+            if ips:
+                return [ips, hosts]
+            else:
+                return None
+        return None
+
+    def __update_hosts(self, hosts):
+        hostslines = []
+        for host in hosts:
+            for ip in host[0]:
+                line = "%s %s" % (ip, " ".join(host[1]))
+                hostslines += [line]
+
+        hostslinesstr = "\n".join(hostslines)
+
+        inblock=False
+        for line in fileinput.input(self.__file, inplace=True):
+            checkline = line.rstrip()
+            if self.__end_marker == checkline:
+                inblock=False
+
+            if True is inblock:
+                checkline = ''
+
+            if self.__start_marker == checkline:
+                inblock=True
+                checkline += "\n%s" % hostslinesstr
+
+            if '' != checkline:
+                print(checkline)
